8f1b3eb8ea0db2d306ac33c9050a05bb077c6c34
Lewis Truong <letruong@csumb.edu>
2018-09-17 20:24:12 +0000

    updated index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <title>Lewis Truong's Index</title>
     </head>
     <body>
+        <p>Lewis Truong's Workspace</p>
         <a href="http://truong-cst336.herokuapp.com/labs/lab_1/">Lab 1</a>
         <a href="http://truong-cst336.herokuapp.com/labs/lab_2/">Lab 2</a>
         <a href="http://truong-cst336.herokuapp.com/homework/hw1/">Homework 1</a>
