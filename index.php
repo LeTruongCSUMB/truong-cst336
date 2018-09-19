63ec5c9d1ac4a5533e03cb4816af629c72dd3118
Lewis Truong <letruong@csumb.edu>
2018-09-19 20:54:32 +0000

    made changes to files

--- index.php
+++ index.php
@@ -5,10 +5,11 @@
     </head>
     <body>
         <p>Lewis Truong's Workspace</p>
+        
         <p>Labs</p>
         <p><a href="http://truong-cst336.herokuapp.com/labs/lab_1/">Lab 1</a></p>
         <p><a href="http://truong-cst336.herokuapp.com/labs/lab_2/777/">Lab 2</a></p>
-        <p><a href="http://truong-cst336.herokuapp.com/labs/lab_3/">Lab 3</a></p>
+        
         <p>Homework</p>
         <p><a href="http://truong-cst336.herokuapp.com/homework/hw1/">Homework 1</a></p>
     </body>
--- labs/Lab_3/silverjack.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Silverjack</title>
-    </head>
-    <body>
-
-    </body>
-</html>
