1f76ec52e85b28f16c06cac175040ba09030052e
Lewis Truong <letruong@csumb.edu>
2018-10-26 20:27:53 +0000

    Added meme lab link to index

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
         <p><a href="http://truong-cst336.herokuapp.com/labs/lab_2/777/">Lab 2</a></p>
         <p><a href="http://truong-cst336.herokuapp.com/labs/lab_4/slider/">Lab 4</a></p>
         <p><a href="http://truong-cst336.herokuapp.com/labs/lab_5/scart/">Lab 5</a></p>
+        <p><a href="http://truong-cst336.herokuapp.com/labs/meme_lab/">Lab 6 (Meme_Lab)</a></p>
         
         <p>Homework</p>
         <p><a href="http://truong-cst336.herokuapp.com/homework/hw1/">Homework 1</a></p>
