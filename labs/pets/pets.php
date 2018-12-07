f6dd34df24411e8508996c879c7e6970edbc83fc
Lewis Truong <letruong@csumb.edu>
2018-12-07 19:42:26 +0000

    Fixing lab 10

--- labs/pets/pets.php
+++ labs/pets/pets.php
@@ -10,7 +10,7 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>   
         <style>
-        @import url("/styles.css");
+            @import url("/styles.css");
             body {
                 text-align: center;
             }
@@ -96,6 +96,8 @@
         }
         ?>
         
+        
+        
         <script>
             
             $(document).ready(function() {
@@ -103,9 +105,9 @@
                     $.ajax({
                     type: "get",
                     url: "adopt.php",
-                    data: {"name": $("#name").text()},
+                    data: {"name": $(this.name).val()},
                     success: function(data,status) {
-                        console.log(data);
+                        console.log(this.name);
                         //alert(status);
                             $("#showName").html(data['name']);
                             $("#showDes").html(
